bdc621cffb39c1474ca79dfc45533fb76fc8edc5
Will Roberts <will.roberts11@gmail.com>
2017-01-26 23:57:44 -0800

    Updates placeholder code for indexer

--- stash/index.go
+++ stash/index.go
@@ -1,11 +1,16 @@
 package stash
 
+// If I decide to build an indexer, this is where it will live. For now, this
+// just contains some relevant quotes from the official documentation.
+
+// From the docs:
 // When a change is made to a stash, the entire stash is sent in an update.
 // If you wish to track historical items, you will need to compare the previous
 // items in the stash to the current items in the stash, otherwise you can
 // simply delete any items matching the stash id and insert the new items.
 // Items don't currently have UIDs. Calculate UIDs based on the stash tab
 // location.
-func updateStash() {
 
+func updateItems(sr *StashResponse) error {
+	return nil
 }
