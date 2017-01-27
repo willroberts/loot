16a52ce8f46c9d760c01717eb7de79e69e719363
Will Roberts <will.roberts11@gmail.com>
2017-01-27 00:00:24 -0800

    Updates comment

--- stash/indexer.go
+++ stash/indexer.go
@@ -3,6 +3,9 @@ package stash
 // If I decide to build an indexer, this is where it will live. For now, this
 // just contains some relevant quotes from the official documentation.
 
+// Judging by some Google results and http://poe.ninja/stats, storing all items
+// from the first page of stashes requires over 4TB of disk space.
+
 // From the docs:
 // When a change is made to a stash, the entire stash is sent in an update.
 // If you wish to track historical items, you will need to compare the previous
